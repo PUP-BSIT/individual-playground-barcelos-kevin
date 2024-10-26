5fd5cf2e646e67536681417d856d3de63a09163a
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-10-26 15:21:42 +0800

    Start

---
+++ list_tuple_dict.py
@@ -0,0 +1,48 @@
+# dict = { "message": "Hello world!" }
+# dict["language"] = "english"
+# print(dict)
+
+# dict = { "message": "Hello world!" }
+# dict["message"] = "Hi There!"
+# print(dict)
+
+# dict = {}
+# print(dict)
+
+# dict = {
+#  "message": "Hello wordl!",
+#  "language": "english",
+#  "response": "Hi there!",
+# }
+
+# for key in dict:
+#   print(key)
+# # Prints message, language, and response
+
+# for key in dict:
+#   print(dict[key])
+
+# response_data = {
+#  "status_code": 200,
+#  "countries": ["Philippines","Japan","China"],
+#  "cities": {
+#    "Philippines": "Manila",
+#    "Japan": "Tokyo",
+#    "Korea": "Seoul",
+#  },
+# }
+# for key in response_data:
+#   print(key)
+# # Prints message, language, and response
+
+# for key in response_data:
+#   print(response_data[key])
+
+# list = [
+#     { "Philippines": "Manila" },
+#     { "Japan": "Tokyo" },
+#     { "Korea": "Seoul" },
+# ]
+
+# print(list[0])
+
