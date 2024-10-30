a0de37ab504bfc0ca11b4362b573fffbd676a42e
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-10-30 10:59:17 +0800

    a

--- list_tuple_dict.py
+++ list_tuple_dict.py
@@ -46,3 +46,25 @@
 
 # print(list[0])
 
+# def sum(num1, num2=5, num3=10):
+#     print (num1, num2, num3)
+#     return num1 + num2 +num3
+
+# print(sum(2))
+
+# def add(*args):
+#     sum = 0
+#     print (args)
+#     for n in args:
+#         sum += n
+#     return sum
+
+# print(add(1, 2, 3, 4, 5))
+# print(add(7, 4, 3))
+# print(add(1, 3, 4, 5))
+
+# def calculate(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"key={key}, value={value}")
+
+# calculate(num_1=3, num_2=5, operator="+")
